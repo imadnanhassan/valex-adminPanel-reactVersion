9b06075965e59a61173203d7df0b0ba7ab468c26
imadnanhassan <abulhassan.dev@gmail.com>
2024-03-04 16:36:09 +0600

    aside bar

--- src/components/HeaderNav/index.jsx
+++ src/components/HeaderNav/index.jsx
@@ -1,14 +1,17 @@
 import { IoIosNotifications } from "react-icons/io";
+import { FaBarsStaggered } from "react-icons/fa6";
+
 import { GoScreenFull } from "react-icons/go";
 
-const HeaderNavbar = () => {
+// eslint-disable-next-line react/prop-types
+const HeaderNavbar = ({ toggleSidebar }) => {
   return (
     <nav className="py-2 px-6 bg-[#f8f4f3] flex items-center shadow-md shadow-black/5 sticky top-0 left-0 z-30">
       <button
         type="button"
         className="text-lg text-gray-900 font-semibold sidebar-toggle "
       >
-        <i className="ri-menu-line" />
+        <FaBarsStaggered onClick={toggleSidebar} />
       </button>
       <div className="bg-[#ffffff] border border-[#F94563] rounded-md flex items-center justify-between ml-6">
         <input
--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,14 +1,23 @@
+import { useState } from "react";
 import HeaderNavbar from "../components/HeaderNav";
 import SideBar from "../components/SideBar";
 import { Outlet } from "react-router-dom";
 
 export default function MainLayout() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   return (
     <div className="flex">
-      <SideBar />
-      <main className="flex-1 w-full  bg-[#F7F7F7] min-h-screen transition-all main">
-        <HeaderNavbar/>
-
+      <SideBar isOpen={isSidebarOpen} />
+      <main
+        className={`flex-1 w-full bg-[#F7F7F7] min-h-screen transition-all ${
+          isSidebarOpen ? "ml-64" : ""
+        }`}
+      >
+        <HeaderNavbar toggleSidebar={toggleSidebar} />
         <Outlet />
       </main>
     </div>
